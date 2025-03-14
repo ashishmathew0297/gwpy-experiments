7770d1e949b08fb0af0eafc818786dfc64014df5
Ashish Thomas Mathew <ashishthomas.mathew@ldas-pcdev2.ligo.caltech.edu>
2025-03-14 03:49:50 -0700

    Changes to Confusion Matrix Visualization.
    Separated out Timeseries loading and added checks for fails
    
    Added code to load TimeSeries samples from dataset and calculate Evaluation Metrics based on the Confusion Matrix

--- modules/statistical_testing/_data.py
+++ modules/statistical_testing/_data.py
@@ -17,7 +17,74 @@ from numpy.typing import NDArray
 
 warnings.filterwarnings('ignore')
 
-def fetch_glitch_data_from_csv(data: pd.DataFrame, gpsTimeKey: str="GPStime", tw: int=5, srate=4096, ifo='L1', begin=0, end=50):
+def get_TimeSeries(gpstime: float, tw: int=5, srate=4096, ifo='L1') -> list:
+    '''
+    This function fetches data from the GWOSC TimeSeries API and stores them in "./glitch_timeseries_data" corresponding to the sample if not already present.
+
+    Inputs:
+    - `gpstime`: The GPS time of the sample
+    - `tw`: Time window to be taken into consideration on either side of the glitch. Thefinal sample returned will have 2.5 seconds removed from either side.
+            Default = 5 seconds 
+    - `srate`: The sampling rate. Default= 4096
+    - `ifo`: The interferometer being studied. Default=L1 (LIGO LivingstonObservatory)
+
+    Outputs:
+    - `unwhitened_noise`: TimeSeries object of the sample
+    - `whitened_noise`: : TimeSeries object of the whitened sample
+    - `q_scan`: q-scan of the sample
+    - `psd`: The calculated power spectral density of the sample
+
+    '''
+
+    filepath = "./glitch_timeseries_data/"
+    filename = f"sample_{ifo}_{gpstime}_{tw}.h5"
+    q_scan = {}
+
+    if not os.path.isdir(filepath):
+        os.mkdir(filepath)
+    
+    # Check if TimeSeries information is already loaded.
+    # Fetch noise data from the LIGO GWOSC if not present
+    if not os.path.isfile(filepath+filename):
+        print(f"Fetching data for GPS time {gpstime} ...")
+        unwhitened_noise = TimeSeries.fetch_open_data(ifo, gpstime - tw,  gpstime + tw, sample_rate=srate)
+        unwhitened_noise.write(filepath+filename)
+    else:
+        unwhitened_noise = TimeSeries.read(filepath+filename)
+
+    unwhitened_noise = unwhitened_noise.to_pycbc()
+
+    # whiten the noise data
+    whitened_noise, psd = unwhitened_noise.whiten(len(unwhitened_noise) / (2 * srate),
+                                len(unwhitened_noise)/( 4 * srate),
+                                remove_corrupted = False,
+                                return_psd = True)
+    
+    # Crop times at each side to avoid border effects
+    whitened_noise = whitened_noise[int(srate * 3):-int(srate * 3)]
+    unwhitened_noise = unwhitened_noise[int(srate * 3):-int(srate * 3)]
+
+    # Creating q-transforms of the data for visualization
+    whitened_noise = TimeSeries(whitened_noise, sample_rate = srate)
+
+    # Stripping the sample down to 1 second of central data
+    # (this might need to be changed for different glitches)
+    unwhitened_noise = unwhitened_noise[int(srate * 1.5):-int(srate * 1.5)]
+    whitened_noise = whitened_noise[int(srate * 1.5):-int(srate * 1.5)]
+
+    # print(f"Calculating q-transform for GPS time {gpstime} ...")
+
+    # Try block added because some glitches throw:
+    # ValueError "Input signal contains non-numerical values"
+    try:
+        q_scan = whitened_noise.q_transform(qrange=[4,64], frange=[10, 2048], tres=0.002, fres=0.5, whiten=False)
+    except ValueError:
+        print(f"Failed to generate q-transform for {gpstime}")
+
+    return unwhitened_noise, whitened_noise, q_scan, psd
+        
+
+def fetch_glitch_data_from_csv(data: pd.DataFrame, gpsTimeKey: str="GPStime", tw: int=5, srate=4096, ifo='L1', begin=0, n_samples=0):
 
     '''
     Fetches the glitch TimeSeries samples from the TimeSeries API, performs the statistical tests on them retruns a datset with the relevant information appended 
@@ -25,12 +92,8 @@ def fetch_glitch_data_from_csv(data: pd.DataFrame, gpsTimeKey: str="GPStime", tw
     Inputs:
     - `data`: Pandas dataframe of glitch data
     - `gpsTimeKey`: The key value for GPS time in the dataset
-    - `srate`: The sampling rate. Default= 4096
-    - `tw`: Time window to be taken into consideration on either side of the glitch. Thefinal sample returned will have 2.5 seconds removed from either side.
-            Default = 5 seconds 
-    - `ifo`: The interferometer being studied. Default=L1 (LIGO LivingstonObservatory)
     - `begin`: Starting index. Default=0
-    - `end`: End index. Default=50
+    - `n_samples`: The number of samples to be taken. Default=0 (loads all the data)
 
     Output: The original input dataset concatenated with the following
     - 'y': Amplitude values of the whitened glitch timeseries
@@ -49,63 +112,80 @@ def fetch_glitch_data_from_csv(data: pd.DataFrame, gpsTimeKey: str="GPStime", tw
     - 'Skew': Skew of the glitch amplitude values
     '''
 
+    # Create a copy of the original dataset, selecting the relevant information to work on
+    data_copy = data[data['ifo'].str.casefold() == ifo.casefold()].copy(deep=True).reset_index(drop=True)
+
     data_readings = {}
-    columns_to_append = ["glitch_timeseries","y","t","q_scan","shapiro_statistic", "shapiro_pvalue","shapiro_prediction","ks_statistic","ks_pvalue","ks_prediction","ad_statistic","ad_critical_values","ad_significance_level","kurtosis","skew"]
 
-    # Select information based on begin and end indexes
-    g_stars = data[gpsTimeKey].iloc[begin:end]
+    # Select the rows based on beginning index and number of samples needed
+    # g_stars = data_copy[gpsTimeKey].iloc[begin:n_samples]
 
-    for i, g_star in enumerate(g_stars):
-        # Fetch noise data from the LIGO GWOSC
-        unwhitened_noise = TimeSeries.fetch_open_data(ifo, g_star - tw,  g_star + tw, sample_rate=srate)
-        unwhitened_noise = unwhitened_noise.to_pycbc()
+    i = 0
 
-        # whiten the noise data
-        whitened_noise, psd = unwhitened_noise.whiten(len(unwhitened_noise) / (2 * srate),
-                                        len(unwhitened_noise)/( 4 * srate),
-                                        remove_corrupted = False,
-                                        return_psd = True)
+    if not n_samples:
+        limit = len(data_copy)
+    else:
+        limit = min(n_samples, len(data_copy))
+
+    while i < limit:
+        unwhitened_noise = []
+        whitened_noise = []
+        q_scan = []
+        psd = 0
         
-        # Crop times at each side to avoid border effects
-        whitened_noise = whitened_noise[int(srate * 3):-int(srate * 3)]
-        unwhitened_noise = unwhitened_noise[int(srate * 3):-int(srate * 3)]
+        g_star = data_copy.iloc[i][gpsTimeKey]
 
-        # Creating q-transforms of the data for visualization
-        whitened_noise = TimeSeries(whitened_noise, sample_rate = srate)
-
-        # Try block added because some glitches throw:
-        # ValueError "Input signal contains non-numerical values"
+        # Try clause is the normal program flow
+        # Except clause skips the current iteration and enters zero values
+        # if TimeSeries fails to load
         try:
-            q_scan = whitened_noise.q_transform(qrange=[4,64], frange=[10, 2048], tres=0.002, fres=0.5, whiten=False)
-        except ValueError:
-            print(f"Failed to load data for {g_star}")
-        
-        # Stripping the sample down to 1 second of central data
-        # (this might need to be changed for different glitches)
-        unwhitened_noise = unwhitened_noise[int(srate * 1.5):-int(srate * 1.5)]
-        whitened_noise = whitened_noise[int(srate * 1.5):-int(srate * 1.5)]
-        
-        t = whitened_noise.times
-        y = whitened_noise.value
+            unwhitened_noise, whitened_noise, q_scan, psd = get_TimeSeries(g_star, tw=tw, srate=srate, ifo=ifo)
+            t = whitened_noise.times
+            y = whitened_noise.value
 
-        # Fetching relevant data to be appended to the input dataframe
-        supplemental_glitch_data = {
-            "glitch_timeseries": unwhitened_noise,
-            "y": y,
-            "t": t,
-            "q_scan": q_scan}
+            # Fetching relevant data to be appended to the input dataframe
+            supplemental_glitch_data = {
+                "glitch_timeseries": unwhitened_noise,
+                "y": y,
+                "t": t,
+                "q_scan": q_scan}
         
-        supplemental_glitch_data.update(generate_sample_statistics(whitened_noise))
+            supplemental_glitch_data.update(generate_sample_statistics(whitened_noise))
+        except ValueError:
+            supplemental_glitch_data = {
+                "glitch_timeseries": np.nan,
+                "y": np.nan,
+                "t": np.nan,
+                "q_scan": np.nan,
+                "shapiro_statistic": np.nan,
+                "shapiro_pvalue": np.nan,
+                "shapiro_prediction": np.nan,
+                "ks_statistic": np.nan,
+                "ks_pvalue": np.nan,
+                "ks_prediction": np.nan,
+                "ad_statistic": np.nan,
+                "ad_critical_values": np.nan,
+                "ad_significance_level": np.nan,
+                "kurtosis": np.nan,
+                "skew": np.nan
+            }
+            print(f"Failed to load data for {g_star}. Glitch Type = {data_copy.iloc[i]['label']}")
 
         for key, value in supplemental_glitch_data.items():
             if key in data_readings:
-                 data_readings[key].append(value)
+                data_readings[key].append(value)
             else:
                 data_readings[key] = [value]
 
+        i = i + 1
+        
     # Append generated statistics to current dataframe and return it        
     dataframe_to_append = pd.DataFrame.from_dict(data_readings, orient='columns').reset_index(drop=True)
-    data_df = pd.concat([data.iloc[begin:end].reset_index(drop=True), dataframe_to_append], axis=1)
+    if n_samples:
+        data_df = pd.concat([data.iloc[begin:n_samples].reset_index(drop=True), dataframe_to_append], axis=1)
+    else:
+        data_df = pd.concat([data.reset_index(drop=True), dataframe_to_append], axis=1)
+
     
     return data_df
 
